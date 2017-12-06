27b5d508924a9716fe77fdc42e3ad177a377422e
Wisemonkey <Oranbusiness@gmail.com>
2017-12-05 21:30:57 -0800

    added javafx bloat shit

--- main.java
+++ main.java
@@ -1,1 +1,13 @@
-//inti
+/*
+ Java Final Project Calculator using raspberry pi
+    by Oran C
+    20171204
+    oranbusiness@gmail.com
+*/
+
+public class Main{
+    public static void main(String[] args) throws InterruptedException
+    {
+        System.out.println("Hello WOrld");
+    }
+}
